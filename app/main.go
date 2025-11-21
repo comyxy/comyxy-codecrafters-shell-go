73be6876fef257d5598932ed0ccf146e08d37860
yuanxiyu.eldlich <yuanxiyu.eldlich@bytedance.com>
2025-11-21 13:36:55 +0800

    [any message]

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 // // Ensures gofmt doesn't remove the "fmt" and "os" imports in stage 1 (feel free to remove this!)
@@ -13,18 +14,45 @@ func main() {
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 
-		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
 
-		command = command[:len(command)-1]
+		input = strings.Trim(input, "\n\r ")
 
-		if command == "exit" {
-			os.Exit(0)
-		}
-
-		fmt.Fprintf(os.Stdout, "%s: command not found\n", command)
+		evaluate(input)
 	}
+}
+
+var COMMANDS = map[string]func(options []string){
+	"exit": exit,
+	"echo": echo,
+}
+
+func evaluate(input string) {
+	args := strings.Split(input, " ")
+
+	cmd, options := args[0], args[1:]
+
+	fn, ok := COMMANDS[cmd]
+	if !ok {
+		fmt.Fprintf(os.Stdout, "%s: command not found\n", cmd)
+		return
+	}
+
+	fn(options)
+}
+
+func exit(options []string) {
+	os.Exit(0)
+}
+
+func echo(options []string) {
+	if len(options) == 0 {
+
+	}
+	r := strings.Join(options, " ")
+	fmt.Fprintln(os.Stdout, r)
 }
